f05f004e42ffda8953005e5dd7790327cc885561
Laiba Haque <130487146+laibahaque@users.noreply.github.com>
2026-03-18 03:50:21 +0500

    Update theme file

---
+++ frontend/src/constants/theme.js
@@ -0,0 +1,18 @@
+const COLORS = {
+
+  background: "#F5F5F5",
+  primary: "#253347",
+  secondary: "#5C708A",
+
+  white: "#FFFFFF",
+  black: "#000000",
+
+  textPrimary: "#253347",
+  textSecondary: "#5C708A",
+
+  border: "#253347",
+  icon: "#bbc8d8"
+
+};
+
+export { COLORS };
